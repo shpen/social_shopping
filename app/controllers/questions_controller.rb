e28d1e5235528a28f07d3faa37bb9f40020dd457
Artem Shvadskiy <shpen72991@gmail.com>
2015-05-15 13:18:33 -0700

    Apply basic bootstrap design

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
 
   # GET /questions
   def index
-    @questions = Question.paginate(page: params[:page])
+    @questions = Question.paginate(page: params[:page], :per_page => 10)
   end
 
   # GET /questions/1
